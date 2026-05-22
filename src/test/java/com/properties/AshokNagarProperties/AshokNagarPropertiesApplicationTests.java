38766f92ff6aa8c7fd762f727a11b1b580d12951
Mohammed-Masood-Ansari <masoodansari1997@gmail.com>
2026-05-22 14:33:38 +0530

    email services has done

---
+++ src/test/java/com/properties/AshokNagarProperties/AshokNagarPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.properties.AshokNagarProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshokNagarPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
